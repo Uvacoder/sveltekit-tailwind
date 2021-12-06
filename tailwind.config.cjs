5cb23cc5760e0fd46f8997af69fa8f4d12c47346
Uvacoder <88749643+Uvacoder@users.noreply.github.com>
2021-12-06 15:41:17 -0500

    Initial commit

---
+++ tailwind.config.cjs
@@ -0,0 +1,12 @@
+module.exports = {
+  mode: 'jit',
+  purge: ["./src/**/*.{html,js,svelte,ts}"],
+  darkMode: 'class', // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: []
+}
